bad04cee4a95dd810ffc155954a38951beb82616
Jemesson <jemesson@sanandres.local>
2017-09-22 08:09:31 -0300

    typescript teste

---
+++ app.ts
@@ -0,0 +1,12 @@
+let message: string = "Help-me, Obi-Wan Kenobi. You're my only hope."
+console.log(message)
+
+let episode: number = 4
+console.log("This is the episode: "+ episode)
+
+episode = episode+1
+console.log("Episode: "+ episode)
+
+let favoritDroid: string
+favoritDroid = "BB-8"
+console.log("My favorite droid is" + favoritDroid)
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+var message = "Help-me, Obi-Wan Kenobi. You're my only hope.";
+console.log(message);
+var episode = 4;
+console.log("This is the episode: " + episode);
+episode = episode + 1;
+console.log("Episode: " + episode);
+var favoritDroid;
+favoritDroid = "BB-8";
+console.log("My favorite droid is" + favoritDroid);
